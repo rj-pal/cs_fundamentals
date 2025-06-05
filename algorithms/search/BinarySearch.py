82d084872fb9fb49b975b938bfaf2a1876e902c7
Robert Pal <92271019+rj-pal@users.noreply.github.com>
2025-06-05 11:30:29 -0400

    Updated search algorithms. Fixed minor bug in string linear search. Updated doc strings and error handling.

--- algorithms/search/BinarySearch.py
+++ algorithms/search/BinarySearch.py
@@ -1,20 +1,33 @@
 def search(iterable, search_item):
-    return binary_search(iterable, search_item, 0, len(iterable) - 1)
+    """
+    Searches for first occurance of given search item over sorted iterable object using binary search algorithm. Type checks 
+    for iterable object before performing the search using interal binary search algorithm.
+
+    Returns the first the occurance of the search item; otherwise -1.
+    """
+    if not hasattr(iterable, "__iter__"):
+        raise TypeError(f"'{type(iterable).__name__}' object is not iterable.")
+    
+    if iterable:
+        type_of_first_item = type(iterable[0])
+        if not isinstance(search_item, type_of_first_item):
+            raise TypeError(f"Search item type mismatch. Expected type '{type_of_first_item.__name__}', got '{type(search_item).__name__}' instead.")
+    
+    return _binary_search(iterable, search_item, 0, len(iterable) - 1)
 
 
-def binary_search(iterable, search_item, left, right):
-    # print(f"Initial Left: {left}, Right: {right}")
+def _binary_search(iterable, search_item, left, right):
+    """Internal binary search function for iterables using recursive method."""
     if left > right:
         return -1
     pointer = left + ((right - left) // 2)
-    # print(f"Left: {left}, Right: {right}, Pointer {pointer}")
     if iterable[pointer] == search_item:
-        # print("HERE")
         if pointer > 0 and iterable[pointer - 1] == search_item:
-            return binary_search(iterable, search_item, left, pointer - 1)
+            return _binary_search(iterable, search_item, left, pointer - 1)
         return pointer
     elif iterable[pointer] < search_item:
-        return binary_search(iterable, search_item, pointer + 1, right)
+        return _binary_search(iterable, search_item, pointer + 1, right)
     else:
-        return binary_search(iterable, search_item, left, pointer - 1)
+        return _binary_search(iterable, search_item, left, pointer - 1)
+
     
--- algorithms/search/LinearSearch.py
+++ algorithms/search/LinearSearch.py
@@ -1,68 +1,35 @@
 def search(iterable, search_item):
     """
-    Searches for a single item element in an iterable and returns its index. Calls find if 
-    seach item is not a single element.
+    Searches for first occurance of given search item over iterable object using linear search algorithm. Type checks for
+    iterable object before performing the search using appropriate internal search algorithm.
 
-    Args:
-        iterable (iterable): The iterable to search through any iterable object. 
-        search_item (any single element): The item to search for in the iterable.
-
-    Returns:
-        int: The index of the first occurrence of `search_item` in `iterable`. 
-             Returns -1 if the `search_item` is not found.
-    """
-
-    """
-    Searches for an element or substring in an iterable and returns its index. Calls custom_find if 
-    iterable is of type 'str'.
-
-    Args:
-        iterable (iterable): The iterable to search through. Can be any iterable object, including strings.
-        search_item (any): The element or substring to search for in the iterable.
-
-    Returns:
-        int: The index of the first occurrence of `search_item` in `iterable`. 
-             Returns -1 if the `search_item` is not found.
-
-    Raises:
-        ValueError: If `search_item` is empty (for strings).
-        TypeError: If `iterable` is not an iterable object or if `search_item` is invalid.
+    Returns the first the occurance of the search item, or 0 for empty string; otherwise -1 
     """
     if not hasattr(iterable, "__iter__"):
         raise TypeError(f"'{type(iterable).__name__}' object is not iterable.")
-    print(search_item)
-    if not search_item:
-        raise ValueError("The 'search_item' must contain at least one element.")
     
     if isinstance(iterable, str):
-        return custom_find(iterable, search_item)
-    
+        return _linear_search_string(iterable, search_item)
+    else:
+        return _linear_search(iterable, search_item)
+
+def _linear_search(iterable, search_item):
+    """Internal linear search function for non-string iterables using brute force method."""    
     for i in range(len(iterable)):
         if iterable[i] == search_item:
             return i
-    else:
-        return -1
-    
-def custom_find(iterable, search_item):
-    """
-    Searches for a substring in a string and returns its starting index.
+    return -1
 
-    Args:
-        iterable (str): The string to search in.
-        search_item (str): The substring to search for.
-
-    Returns:
-        int: The index of the first occurrence of `search_item` in `iterable`. 
-             Returns -1 if `search_item` is not found.
-
-    Raises:
-        TypeError: If `iterable` is not a string.
-    """
+def _linear_search_string(iterable, search_item):
+    """Internal linear search function for string iterables using brute force method.."""
     if not isinstance(iterable, str):
         raise TypeError(f"'{type(iterable).__name__}' object is not of type 'str'.")
     
+    if not isinstance(search_item, str):
+        raise TypeError(f"'{type(search_item).__name__}' object is not of type 'str'.")
+    
     if not search_item:
-        raise ValueError("The 'search_item' must contain at least one element.")
+        return 0
     
     if len(search_item) > len(iterable):
         return -1
@@ -72,12 +39,6 @@ def custom_find(iterable, search_item):
             j = 0
             while j < len(search_item) and iterable[i + j] == search_item[j]:
                     j += 1
-            else:
-                return i
-    else:
-        return -1
-    
-# a=custom_find(["aaa", "bbb", "b", "b", "b"], "bbb")
-# a = search('ontario', 'ntooooooooo')
-# a = search(256,2)
-# print(a)
+                    if j == len(search_item):
+                        return i
+    return -1
