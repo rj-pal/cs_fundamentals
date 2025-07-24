8c7e3ff1825c19a71d64163220cb926dd14148a9
Robert Pal <92271019+rj-pal@users.noreply.github.com>
2025-07-23 21:53:03 -0400

    Added doc string

--- algorithms/search/LinearSearch.py
+++ algorithms/search/LinearSearch.py
@@ -1,44 +1,76 @@
-def search(iterable, search_item):
+def search(iterable, search_element):
     """
-    Searches for first occurance of given search item over iterable object using linear search algorithm. Type checks for
-    iterable object before performing the search using appropriate internal search algorithm.
+    Searches for first occurance of given search element over iterable object using linear search algorithm. 
+    
+    Args:
+        Iterable object (list)
+        Element to be searched 
+        
+    Returns:
+        Index of first occurance of search element, 0 for empthy string; otherwise -1
+            
+    Raises:
+        TypeError: If the iterable object is not iterable
 
-    Returns the first the occurance of the search item, or 0 for empty string; otherwise -1 
+    Returns the first the occurance of the search element, or 0 for empty string; otherwise -1 
     """
     if not hasattr(iterable, "__iter__"):
         raise TypeError(f"'{type(iterable).__name__}' object is not iterable.")
     
     if isinstance(iterable, str):
-        return _linear_search_string(iterable, search_item)
+        return _linear_search_string(iterable, search_element)
     else:
-        return _linear_search(iterable, search_item)
+        return _linear_search(iterable, search_element)
 
-def _linear_search(iterable, search_item):
-    """Internal linear search function for non-string iterables using brute force method."""    
+def _linear_search(iterable, search_element):
+    """
+    Internal linear search function for non-string iterables using brute force method.
+
+    Args:d
+        Iterable object (list, tuple, set, but not string)
+        Element to be searched 
+        
+    Returns:
+        Index of first occurance of search element, or -1 if not found.
+    
+    """    
     for i in range(len(iterable)):
-        if iterable[i] == search_item:
+        if iterable[i] == search_element:
             return i
     return -1
 
-def _linear_search_string(iterable, search_item):
-    """Internal linear search function for string iterables using brute force method.."""
+def _linear_search_string(iterable, search_element):
+    """
+    Internal linear search function for string iterables using brute force method or niave algorithm.
+
+    Args:
+        Iterable object (str)
+        Element to be searched (str)
+        
+    Returns:
+        Index of first occurance of search element, 0 for empty search string, or -1 if not found.
+            
+    Raises:
+        TypeError: If the iterable object is not a string
+        TypeError: If the search element is not a string
+    """
     if not isinstance(iterable, str):
         raise TypeError(f"'{type(iterable).__name__}' object is not of type 'str'.")
     
-    if not isinstance(search_item, str):
-        raise TypeError(f"'{type(search_item).__name__}' object is not of type 'str'.")
+    if not isinstance(search_element, str):
+        raise TypeError(f"'{type(search_element).__name__}' object is not of type 'str'.")
     
-    if not search_item:
+    if not search_element:
         return 0
     
-    if len(search_item) > len(iterable):
+    if len(search_element) > len(iterable):
         return -1
     
-    for i in range(len(iterable) - len(search_item) + 1):
-        if iterable[i] == search_item[0]:
+    for i in range(len(iterable) - len(search_element) + 1):
+        if iterable[i] == search_element[0]:
             j = 0
-            while j < len(search_item) and iterable[i + j] == search_item[j]:
+            while j < len(search_element) and iterable[i + j] == search_element[j]:
                     j += 1
-                    if j == len(search_item):
+                    if j == len(search_element):
                         return i
     return -1
